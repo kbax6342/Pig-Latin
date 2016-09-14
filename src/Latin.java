77cb1804bc8e598098b8f9f7d8b4d2af76ee5d29
Kevin Baxter <kbax6342@gmail.com>
2016-09-14 15:48:58 -0400

    Pig Latin Works

--- src/Latin.java
+++ src/Latin.java
@@ -1,37 +1,95 @@
 import java.util.Scanner;
-
+ 
 
 public class Latin {
 
 	public static void main(String[] args) {
-		// The application prompts the user for a word
-		//The application translates the text to Pig Latin and displays it on the console
-		//The application asks the user if he or she wants to translate another word
+		//Translate the Words
+		//Take in String Sentence
+		//Break up the String
+		
 		
 		
 		//Importing classes 
 		Scanner input = new Scanner (System.in);
-		StringBuffer strBuff = new StringBuffer();
 		
-		//Variables Needed to make additions to String
-		String add = "way";
-		String secondAdd = "ay";
-		String data;
+		int indexOfVowel = -1;
 		
 		
-
+		
 		//Saying Welcome to the user
 		System.out.println("Welcome to the Pig Latin Translator!");
 		
 		//Asking the user to put in a line to be translated
 		System.out.println("Enter a line to be translated:  ");
-		strBuff.append(input.nextLine());
+		//Taking the input and assigning and it a new variable
+		String text = input.nextLine();
+		text = text.toLowerCase();
 		
-		 StringBuffer finalSentence = methods.lowerCase(strBuff);
+		if(text.charAt(0) == 'a' || text.charAt(0) == 'e' || text.charAt(0) == 'i' || text.charAt(0) == 'o' || text.charAt(0) == 'u'){
+			System.out.println(text + "ay");
+		}else{
+		
+		
+		for(int x = 0; x <= text.length(); x++){
+			if(text.charAt(x) == 'a' || text.charAt(x) == 'e' || text.charAt(x) == 'i' || text.charAt(x) == 'o' || text.charAt(x) == 'u'){
+				 indexOfVowel = x;
+				break;
+			}
+			
+		}
+		String backSentence = text.substring(indexOfVowel);
+		String frontSentence = text.substring(0, indexOfVowel);
+		
+		System.out.println(backSentence + frontSentence + "way");
+		}
+	
 		
 		
 		
-		System.out.println(finalSentence);
+		
+		
+		
+		
+		
+		
+		
+		
+		//identifies letter in first place
+//		int vowelFirst = vow.indexOf(firstLetter);//determines if first letter is a vowel
+//		//any letters before the first vowel
+//		String firstChunk =
+//		
+//		boolean firstLetterVowel = (vowelFirst >= 0);//moves characters
+//		String ext = text.substring(1); //captures word without first letter - get it?
+//		
+//		if (firstLetterVowel) {
+//			System.out.println(text + firstLetter + "way");
+//		}// end if
+//		else {
+//			System.out.println(ext + firstLetter +  "ay");
+//		}//end else
+////		System.out.println("Continue? y/n");
+////		input.nextLine();
+////		choice = input.nextLine();
+//		
+//		//Spliting the string into seperate words
+//		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	
+	
+		 
+		
+		
+	
 	}
 
 
